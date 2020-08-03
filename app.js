54dab384695429f0210d9de7f2e58358915373f2
vasya <ololohkalool@gmail.com>
2020-08-03 13:39:06 +0300

    write code in app.js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+const modalOverlay = document.querySelector('.modal-overlay'),
+	  openBtn = document.querySelector('.header-btn'),
+	  closeBtn = document.querySelector('.close-btn');
 
 
-
-
+window.addEventListener('click', e => {
+	let target = e.target.classList;
+	if(target.contains('header-btn')) {
+		modalOverlay.classList.add('open-modal');
+	} else if (target.contains('close-btn') || target.contains('open-modal')) {
+		modalOverlay.classList.remove('open-modal');
+	}
+});
